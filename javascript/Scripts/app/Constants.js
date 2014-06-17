d13211f8dce3fe18d0f3fb9f47e96274a8149a21
malibeg <malibeg@hotmail.com>
2014-06-17 14:13:36 +0200

    refactoring 2

---
+++ javascript/Scripts/app/Constants.js
@@ -0,0 +1,9 @@
+﻿var LetterGame = LetterGame || {};
+
+LetterGame.constants = {
+    lettercolor: '#00CC22',
+    clickcolor: '#9999FF',
+    oddfieldcolor: '#BBFFBB',
+    pairfieldcolor: '#669966',
+    misscolor: '#EE3333'
+};
